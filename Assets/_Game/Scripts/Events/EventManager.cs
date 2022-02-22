37ac041e7f0306773c8f9023cd717f53f46107ba
Shaladdin <abyboyganteng@gmail.com>
2022-02-22 21:52:29 +0700

    Combat system donee, just need to add event when the enemy die for dropping loot want to add that now but my father gonna kill me if im not in the bed yet, cya be back in less then 7 hours ig

--- Assets/_Game/Scripts/Events/EventManager.cs
+++ Assets/_Game/Scripts/Events/EventManager.cs
@@ -12,12 +12,17 @@ public class EventManager
     public delegate void D_Inventory(InventoryObject inventory);
     public delegate void D_ItemWithGameObject(ItemObject item, GameObject obj);
 
+    public delegate void D_GameObjectWithInt(GameObject obj, int dmg);
 
+
+    public static event D_GameObjectWithInt onDamageActor;
     public static event D_Inventory onInventoryChanged;
     public static event D_ItemWithGameObject onItemUse;
+
 
 
     public static void ItemUse(ItemObject item, GameObject obj) { onItemUse?.Invoke(item, obj); }
 
     public static void InventoryChanged(InventoryObject inventory) { onInventoryChanged?.Invoke(inventory); }
+    public static void DamageActor(GameObject obj, int dmg) { onDamageActor?.Invoke(obj, dmg); }
 }
