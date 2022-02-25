c88ed843b8a55181aefad0f2a189709f65c2d4c0
Zachary Krizo <zachkrizo@gmail.com>
2022-02-24 22:21:55 -0800

    Game Over UI
    Stubbed in some Game Over UI ready to use. Just need to hook up retry/checkpoint logic if/when we do that

---
+++ Assets/_Game/Scripts/States/GameOverState.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverState : BaseState
+{
+    public readonly string UI_PREFAB = "UIGameOver";
+    private UIWidget _uiWidget;
+    private GameSystems _systems;
+
+    public GameOverState(GameSystems systems)
+    {
+        _systems = systems;
+    }
+
+    public override void Transition(State toState, BaseState prevStateClass = null, Dictionary<string, object> options = null)
+    {
+        // Guard against same state transition
+        if (toState == CurrentState)
+        {
+            return;
+        }
+
+        if(toState == State.Active && CurrentState == State.Inactive)
+        {
+            SetupState();
+        }
+        else if(toState == State.Inactive && CurrentState == State.Active)
+        {
+            TeardownState();
+        }
+        else if(toState== State.Background && CurrentState == State.Active)
+        {
+            if (_uiWidget != null) 
+            { 
+                _uiWidget.UIObject.SetActive(false);
+            }
+        }
+        else if(toState == State.Active && CurrentState == State.Background)
+        {
+            if (_uiWidget != null)
+            {
+                _uiWidget.UIObject.SetActive(true);
+            }
+        }
+
+        CurrentState = toState;
+    }
+
+    public void SetupState()
+    {
+        _uiWidget = _systems.UIManager.LoadUI(UI_PREFAB);
+        _uiWidget.UIObject.GetComponent<UIGameOver>()?.Setup(_systems);
+    }
+
+    public void TeardownState()
+    {
+        if(_uiWidget != null)
+        {
+            _systems.UIManager.RemoveUIByGuid(_uiWidget.GUID);
+        }
+    }
+}
