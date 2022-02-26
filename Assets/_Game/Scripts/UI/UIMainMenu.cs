938b01f9149b7f9c22ce3a10a3ffceedc41aa13a
Zachary Krizo <zachkrizo@gmail.com>
2022-02-26 13:44:32 -0800

    Debug Jump Scene
    Added debug ability to jump to scenes to main menu

--- Assets/_Game/Scripts/UI/UIMainMenu.cs
+++ Assets/_Game/Scripts/UI/UIMainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -36,4 +37,63 @@ public class UIMainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+#if DEBUG
+    [SerializeField] private GameObject _debugMenuGroup;
+    public enum JumpState
+    {
+        Forest1State,
+        Forest2State,
+        TavernState,
+        GhostForest1State,
+        GhostForest2State,
+        Dungeon1State,
+        Dungeon2State,
+        Apartment1State,
+        Apartment2State,
+        Apartment3State,
+    }
+    public void DEBUG_ToggleDisplayDebugMenu()
+    {
+        _debugMenuGroup.SetActive(!_debugMenuGroup.activeSelf);
+    }
+
+    public void DEBUG_JumpToState(string stateString)
+    {
+        JumpState state = (JumpState)Enum.Parse(typeof(JumpState), stateString);
+        switch(state)
+        {
+            case JumpState.Forest1State:
+                _systems.StateManager.NavigateToState(typeof(Forest1State));
+                break;
+            case JumpState.Forest2State:
+                _systems.StateManager.NavigateToState(typeof(Forest2State));
+                break;
+            case JumpState.TavernState:
+                _systems.StateManager.NavigateToState(typeof(TavernState));
+                break;
+            case JumpState.GhostForest1State:
+                //_systems.StateManager.NavigateToState(typeof());
+                break;
+            case JumpState.GhostForest2State:
+                //_systems.StateManager.NavigateToState(typeof());
+                break;
+            case JumpState.Dungeon1State:
+                //_systems.StateManager.NavigateToState(typeof());
+                break;
+            case JumpState.Dungeon2State:
+                //_systems.StateManager.NavigateToState(typeof());
+                break;
+            case JumpState.Apartment1State:
+                _systems.StateManager.NavigateToState(typeof(Apartment1State));
+                break;
+            case JumpState.Apartment2State:
+                _systems.StateManager.NavigateToState(typeof(Apartment2State));
+                break;
+            case JumpState.Apartment3State:
+                _systems.StateManager.NavigateToState(typeof(Apartment3State));
+                break;
+        }
+    }
+#endif
 }
