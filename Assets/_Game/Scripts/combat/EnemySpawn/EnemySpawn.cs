fd67b6c684b368abfe1ad1b37f60e24a8374cb84
Patrick <Patrick@Moffett.dev>
2022-02-26 16:58:47 -0800

    Auto stash before rebase of "origin/develop"

---
+++ Assets/_Game/Scripts/combat/EnemySpawn/EnemySpawn.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public enum EditorIcon{Slime, Skeleton, Crow,Ghost,Mushroom,Spider}
+
+    public GameObject PrefabToSpawn;
+    public bool ShouldRespawn = true;
+    public float RespawnTime = 5f;
+    public EditorIcon displayIcon = EditorIcon.Slime;
+
+    private GameObject LastSpawnedEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+    private void OnEnable()
+    {
+        EventManager.onObjectDied += OnObjectDied;
+    }
+    private void OnObjectDied(GameObject objectThatDied)
+    {
+        if(LastSpawnedEnemy != null ? objectThatDied == LastSpawnedEnemy : false)
+        {
+            if (ShouldRespawn == true)
+            {
+                StartCoroutine(RespawnEnemy(RespawnTime));
+            }
+        }
+    }
+    
+    private IEnumerator RespawnEnemy(float timeToRespawn)
+    {
+        yield return new WaitForSeconds(timeToRespawn);
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        
+        LastSpawnedEnemy = Instantiate(PrefabToSpawn, transform.position, Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position,new Vector3(1f,1f,1f));
+        Gizmos.color = Color.white;
+        switch (displayIcon)
+        {
+            case EditorIcon.Slime:
+                Gizmos.DrawIcon(transform.position, "slime.tiff", true);
+                break;
+            case EditorIcon.Spider:
+                Gizmos.DrawIcon(transform.position, "spider.tiff", true);
+                break;
+            case EditorIcon.Mushroom:
+                Gizmos.DrawIcon(transform.position, "mushroom.tiff", true);
+                break;
+            case EditorIcon.Ghost:
+                Gizmos.DrawIcon(transform.position, "ghost.tiff", true);
+                break;
+            case EditorIcon.Crow:
+                Gizmos.DrawIcon(transform.position, "crow.tiff", true);
+                break;
+            case EditorIcon.Skeleton:
+                Gizmos.DrawIcon(transform.position, "skeleton.tiff", true);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
